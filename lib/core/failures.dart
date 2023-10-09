9b029292b5d99df05c29fdf03ae7ca85fd60a5c0
Praveen <praveenpathmakaran@gmail.com>
2023-10-09 13:58:46 +0530

    concrete number usecases refactoring done

--- lib/core/failures.dart
+++
@@ -1,7 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class Failures extends Equatable {
-  const Failures([List properties = const <dynamic>[]]);
-  @override
-  List<Object?> get props => [];
-}
--- lib/features/domain/repositories/number_trivia_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:numbertrivia/core/failures.dart';
-import 'package:numbertrivia/features/domain/entities/number_trivia.dart';
-
-abstract class NumberTriviaRepository {
-  Future<Either<Failures, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failures, NumberTrivia>> getRandomNumberTrivia();
-}
